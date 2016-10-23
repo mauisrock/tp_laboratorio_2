652e89d4e7585e118cd8985e1afac6d27653a8a1
mauisrock <mauisrock@hotmail.com>
2016-10-23 12:15:14 -0300

    Add files via upload

---
+++ TP_2/Alumno_Clase_12/Clase_12_Library/Camion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Clase_12_Library_2;
+using System.Threading.Tasks;
+
+namespace Clase_12_Library
+{
+    public class Camion : Vehiculo
+    {
+        public Camion(Vehiculo.EMarca marca, string patente, ConsoleColor color)
+            : base(marca, patente, color)
+        {
+        }
+        /// <summary>
+        /// Los camiones tienen 8 ruedas
+        /// </summary>
+        public override short CantidadRuedas
+        {
+            get
+            {
+                return 8;
+            }
+        }
+        /// <summary>
+        /// Muestra las caracteristicas de camion
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CAMION");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("RUEDAS : {0}", this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP_2/Alumno_Clase_12/Clase_12_Library/Vehiculo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_12_Library_2
+{
+    public abstract class Vehiculo
+    {
+        public enum EMarca
+        {
+            Yamaha, Chevrolet, Ford, Iveco, Scania, BMW
+        }
+
+        EMarca _marca;
+        string _patente;
+        ConsoleColor _color;
+
+        // <summary>
+        // Retornará la cantidad de ruedas del vehículo
+        // </summary>
+        public abstract short CantidadRuedas { get; }
+        /// <summary>
+        /// Constructor de Vehiculo.
+        /// </summary>
+        /// <param name="marca">Marca del Vehiculo</param>
+        /// <param name="patente">Patente del Vehiculo</param>
+        /// <param name="color">Color del Vehiculo</param>
+        public Vehiculo(EMarca marca, string patente, ConsoleColor color)
+        {
+            this._color = color;
+            this._marca = marca;
+            this._patente = patente;
+        }
+        public virtual string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("PATENTE: {0}\r\n", this._patente);
+            sb.AppendFormat("MARCA  : {0}\r\n", this._marca.ToString());
+            sb.AppendFormat("COLOR  : {0}\r\n", this._color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehículos son iguales si comparten la misma patente
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            bool retorno = false;
+            if (v1._patente.Equals(v2._patente))
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+        /// <summary>
+        /// Dos vehículos son distintos si su patente es distinta
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1 == v2);
+        }
+    }
+}
